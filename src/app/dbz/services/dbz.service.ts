717a944c0dc478df02b533f9a9c350a65916f5ce
MaxAguiwindows <manuelmaximo19@gmail.com>
2023-09-27 19:58:09 -0500

    CREATE: dist

---
+++ src/app/dbz/services/dbz.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { v4 as uuid } from "uuid";
+import { Character } from '../interfaces/character.interface';
+
+console.log(uuid())
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DbzService {
+  public characters: Character[] = [
+    {
+      id: uuid(),
+      name: 'krillin',
+      power: 1000
+    },
+    {
+      id: uuid(),
+      name: 'Goku',
+      power: 1500
+    },
+    {
+      id: uuid(),
+      name: 'goten',
+      power: 254
+    }
+  ];
+
+  addCharacter( character:Character):void{
+    const newCharacter: Character = {  ...character ,id: uuid() }
+
+    this.characters.push(newCharacter)
+  }
+
+  deleteCharacterById(id:string):void{
+    console.log('Main Page',id)
+    this.characters = this.characters.filter( character => character.id !==  id);
+  }
+
+}
